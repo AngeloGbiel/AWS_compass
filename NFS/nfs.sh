911686320f9a667fb2addf86239e4117cf485e61
Angelo <angelog.iff@gmail.com>
2023-07-09 14:34:33 -0300

    Configuração Do NFS dentro da instância EC2

---
+++ NFS/nfs.sh
@@ -0,0 +1,8 @@
+#
+#   serviço de NSF
+#   Escolher o nome e a vpc
+#   Selecionar para os Destinos de montagem a mesma security group da instância
+#   Entrar no diretório do usuário com o comando "cd" e depois criar um arquivo com o comando sudo mkdir nfs
+#   Montar o nfs 
+#   Abrir outra instância e visualizar a sincronização   
+#
